812a5f5734a76d0b5878c0c6cb59d0440917f911
Andressa Araujo <andressa.araujo@dextra-sw.com>
2019-02-13 16:13:02 -0200

    html structure

--- src/app/components/app.module.ts
+++ src/app/components/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TransportChargingModule } from '../transport-charging/transport-charging.module';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app-route';
+import { LoginModule } from '../login/login.module';
+import { RegisterModule } from '../register/register.module';
+import { TravelRequestModule } from '../travel-request/travel-request.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +14,11 @@ import { TransportChargingModule } from '../transport-charging/transport-chargin
   ],
   imports: [
     BrowserModule,
-    TransportChargingModule
+    TransportChargingModule,
+    RouterModule.forRoot(ROUTES),
+    LoginModule,
+    RegisterModule,
+    TravelRequestModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Route, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  signin() {
+    this.router.navigateByUrl('travel-request');
+  }
+  signup() {
+    this.router.navigateByUrl('register');
+  }
 }
